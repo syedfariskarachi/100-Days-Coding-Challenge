57afc6225c2353eeb6a2d3cb73b10a9ae8eae283
Faris Shah <132473241+syedfariskarachi@users.noreply.github.com>
2024-06-29 00:28:30 +0500

    100-days-of-coding-challenge

---
+++ day_1.ts
@@ -0,0 +1,19 @@
+// Question 1: Install Node.js, TypeScript and VS Code on your computer/Laptop.
+
+//   Done
+
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+
+let _name: string = "faris"; // store a person name
+
+console.log(
+  "Hello " + _name + ", would you like to learn some Typescript today?"
+); //print message
+
+// Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let _name2: string = "faris"; // save the person name,
+
+console.log(_name2.toLowerCase()); // print in lower case ,
+console.log(_name2.toUpperCase()); // print in upper case ,
+console.log(_name2.charAt(0).toUpperCase() + _name2.slice(1).toLowerCase()); // print first letter in uppercase
