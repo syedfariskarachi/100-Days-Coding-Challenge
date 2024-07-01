039acbed2496d54c04e17ad327789812b87ea386
Faris Shah <132473241+syedfariskarachi@users.noreply.github.com>
2024-07-01 21:46:53 +0500

    100-days-coding-challenge

--- day_8.ts
+++ day_8.ts
@@ -7,35 +7,35 @@ console.log(array[2])  // print 3rd item of the array
 // Question 23: Conditional Tests: Write a series of conditional tests. Predict the results of each test.
 
 let name:string="Faris Shah"
-console.log("Name : Yaseen Shah ;",name=="Yassen Shah")
+console.log("Name : Yaseen Shah ;",name=="Yassen Shah")  //false
 console.log("Name : Faris Shah ;",name=="Faris Shah")
 let city:string ="Karachi"
-console.log("City : Lahore ;",city=="Lahore")
-console.log("City : Karachi ;",city=="Karachi")
+console.log("City : Lahore ;",city=="Lahore")   //false
+console.log("City : Karachi ;",city=="Karachi")  //true
 let age:number = 18
-console.log("Age : 23 ;",age==23)
-console.log("Age : 18 ;",age==18)
+console.log("Age : 23 ;",age==23)  //false
+console.log("Age : 18 ;",age==18)  //true
 let class_:string="xi"
-console.log("class : x ;",class_=="x")
-console.log("class : xi ;",class_=="xi")
+console.log("class : x ;",class_=="x")  //false
+console.log("class : xi ;",class_=="xi") //true
 let roll_number:number=90665
-console.log("Roll number : 11654 ;",roll_number==11654)
-console.log("Roll number : 90665 ;",roll_number==90665)
+console.log("Roll number : 11654 ;",roll_number==11654)  //false
+console.log("Roll number : 90665 ;",roll_number==90665)  //true
 
 // Question 24: More Conditional Tests: Expand your conditional tests to cover a wider range of comparisons.
 
 //equlity with string
-console.log("Testing Equlity with string:","PUBG"=="PUBG")
-console.log("Equlity with string:",12==12)
+console.log("Testing Equlity with string:","PUBG"=="PUBG")  //true
+console.log("Equlity with string:",12==12)  //true
 //numerical test
-console.log("Numerical Test:",7>10)
-console.log("Numerical Test:",3<4)
+console.log("Numerical Test:",7>10) //false
+console.log("Numerical Test:",3<4)  //true
 // and(&&) or(|) test
-console.log(true&&false)
-console.log(true||false)
+console.log(true&&false)  //false
+console.log(true||false)  //true
 //test item is in a array
 let car:string[] = ["Toyota","Audi","BMW","Honda"]
-console.log(car.includes("Ferari"))
-console.log(car.includes("BMW"))
+console.log(car.includes("Ferari" ))  //false
+console.log(car.includes("BMW"))   //true
 //test item is not in a array
-console.log(!car.includes("Bently"))
+console.log(!car.includes("Bently"))  //true
