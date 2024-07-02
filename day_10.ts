13c7f608cd2c4da3f8c48ebdef8bda452daa2527
Faris Shah <132473241+syedfariskarachi@users.noreply.github.com>
2024-07-03 00:03:35 +0500

    100-days-coding-challenge

--- day_10.ts
+++ day_10.ts
@@ -1,58 +1,52 @@
 // Question 28: Stages of Life: Determine a person’s life stage with an if-else chain.
-let age:number= 17
-if (age<=3){
-    console.log("You are a Toddler")
-}
-else if(age<=5){
-    console.log("You are a Preschooler")
-}
-else if(age<=12){
-    console.log("You are a child")
-}
-else if(age<=20){
-    console.log("You are a Teenager")
-}
-else if(age<=39){
-    console.log("You are a Young Adult")
-}
-else if(age<=59){
-    console.log("You are a Middle Aged Adult")
-}
-else if(age<=60){
-    console.log("You are a Senior")
+let age: number = 17;
+if (age <= 3) {
+  console.log("You are a Toddler");
+} else if (age <= 5) {
+  console.log("You are a Preschooler");
+} else if (age <= 12) {
+  console.log("You are a child");
+} else if (age <= 20) {
+  console.log("You are a Teenager");
+} else if (age <= 39) {
+  console.log("You are a Young Adult");
+} else if (age <= 59) {
+  console.log("You are a Middle Aged Adult");
+} else if (age <= 60) {
+  console.log("You are a Senior");
 }
 
 // Question 29: Favorite Fruit: Create an array for your favorite fruits and check if certain fruits are included.
 
-let fav_fruits:string[]=["Apple","Mango","Banana","Strawberry"]
-if(fav_fruits.includes("Grapes")){
-    console.log("Enjoy the burst of flavor from every grape.")
-}else if(fav_fruits.includes("Pineapple")){
-    console.log("Tropical and tangy, pineapples are a taste of paradise.")
-}else if(fav_fruits.includes("Blueberry"))
-{
-    console.log("Add a handful of blueberries to your breakfast for a healthy boost.")
-}else if(fav_fruits.includes("Watermelon")){
-    console.log("Stay hydrated with the juicy goodness of watermelon")
-
-}else if(fav_fruits.includes("Apple")){
-    console.log("An apple a day keeps the doctor away.")
+let fav_fruits: string[] = ["Apple", "Mango", "Banana", "Strawberry"];
+if (fav_fruits.includes("Grapes")) {
+  console.log("Enjoy the burst of flavor from every grape.");
+} else if (fav_fruits.includes("Pineapple")) {
+  console.log("Tropical and tangy, pineapples are a taste of paradise.");
+} else if (fav_fruits.includes("Blueberry")) {
+  console.log(
+    "Add a handful of blueberries to your breakfast for a healthy boost."
+  );
+} else if (fav_fruits.includes("Watermelon")) {
+  console.log("Stay hydrated with the juicy goodness of watermelon");
+} else if (fav_fruits.includes("Apple")) {
+  console.log("An apple a day keeps the doctor away.");
 }
 
-if (fav_fruits.includes("Mango")){
-    console.log("Lush and juicy, mangoes are the king of fruits")
+if (fav_fruits.includes("Mango")) {
+  console.log("Lush and juicy, mangoes are the king of fruits");
 }
-if(fav_fruits.includes("Banana")){
-    console.log("Rich in potassium, bananas are great for muscle health")
+if (fav_fruits.includes("Banana")) {
+  console.log("Rich in potassium, bananas are great for muscle health");
 }
 
 // Question 30: Hello Admin: Greet users differently, especially 'admin'.
-let admin:string[]=["Faris shah","user2","user3"]
+let admin: string[] = ["Faris shah", "user2", "user3"];
 
-admin.forEach(admin=>{
-    if(admin=="Faris shah"){
-        console.log("Hello, Faris would you like to see Hamid Status report?" )
-    }else{
-        console.log(`Hello ${admin}, thank you for logging again`)
-    }
-})
+admin.forEach((admin) => {
+  if (admin == "Faris shah") {
+    console.log("Hello, Faris would you like to see Hamid Status report?");
+  } else {
+    console.log(`Hello ${admin}, thank you for logging again`);
+  }
+});
---
+++ day_11.ts
@@ -0,0 +1,49 @@
+// Question 31: No Users: Ensure your user list isn’t empty.
+
+let user: string[] = [];
+if (user.length === 0) {
+  console.log(`Today, ${user.length} users have logged out`);
+} else {
+  console.log(`Today, ${user.length} users have created new accounts`);
+}
+
+// Question 32: Checking Usernames: Ensure uniqueness in usernames.
+
+let users: string[] = ["user1", "user2", "Faris", "user3", "user4"];
+let new_users: string[] = [
+  "faris",
+  "user1",
+  "user2",
+  "user3",
+  "Hassan",
+  "user5",
+];
+new_users.forEach((newusers) => {
+  if (
+    users.some(
+      (existingUser) => existingUser.toLowerCase() === newusers.toLowerCase()
+    )
+  ) {
+    console.log(`${newusers}, please enter a new user name.`);
+  } else {
+    console.log(`${newusers} is available.`);
+  }
+});
+
+// Question 33: Ordinal Numbers: Display numbers with their ordinal suffixes.
+let number: number[] = [1, 2, 3, 4, 6, 8];
+number.forEach((number) => {
+  let suffix = "o";
+  if (number == 1) {
+    suffix = "st";
+  } else if (number == 2) {
+    suffix = "nd";
+  } else if (number == 3) {
+    suffix = "rd";
+  } else if (number == 4) {
+    suffix = "rth";
+  } else if (number <= 10) {
+    suffix = "th";
+  }
+  console.log(`${number}${suffix}`);
+});
