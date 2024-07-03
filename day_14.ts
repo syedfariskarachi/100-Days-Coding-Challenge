f90388ee605eb92e52cbb1b0488fc8bfd2c3067a
Faris Shah <132473241+syedfariskarachi@users.noreply.github.com>
2024-07-03 10:40:49 +0500

    👉 update 😃

---
+++ day_14.ts
@@ -0,0 +1,34 @@
+// Question 40: Album: Create objects for music albums.
+function albums(artist:string, title:string, track?:number) {
+    let album = { artist, title };
+    if (track) {
+          album[`track`] = track;
+    }
+    return album;
+  }
+  console.log(albums("artist 1","first album"))
+  console.log(albums("artist 2","second album"))
+  console.log(albums("artist 3","third album"))  
+   
+// Question 41: Magicians: Display magician names from an array.
+ 
+  let magicians: string[] = ["Alice", "David", "Chris"];
+
+  function show_magicians(magicians: string[]) {
+      magicians.forEach(magician => {
+          console.log(magician);
+      });
+  }
+  
+  show_magicians(magicians);
+// Question 42: Great Magicians: Add "the Great" to magician names.
+
+function make_great(magicians:string[]){
+    for (let i = 0; i < magicians.length; i++) {
+        magicians[i] = magicians[i] + " the great"
+        
+    }
+
+}
+make_great(magicians)
+show_magicians(magicians)
